53940a1c2f30b26ec9f077fa8fb11bb4f7c53705
IceMelisa <isorok1n1@yandex.ru>
2025-01-23 15:54:41 +0500

    four

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import sqlite3
 from flask import (
     Flask,
     render_template,
     redirect,
-    request
+    request,
+    url_for
 )
+connection = sqlite3.connect('users.db')
+cursor = connection.cursor()
+
+
 app = Flask(__name__)
 
 
@@ -17,8 +23,21 @@ def get_log():
     return render_template('log.html')
 
 
+@app.route('/home', methods=['GET','POST'])
+def get_home():
+    return render_template('home.html')
+
+
+
 @app.route('/reg', methods=['GET','POST'])
 def get_reg():
+    if request.method == 'POST':
+        login = request.form.get('login', type=str)
+        password = request.form.get('password', type=str)
+        if login == 'admin' and password == 'admin':
+            return redirect(url_for('get_home', login=login, password=password))
+        else:
+            return "<h1>Ебать ты хуй</h1>"
     return render_template('reg.html')
 
 
